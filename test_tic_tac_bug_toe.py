512575acc11d5e2480cb067a73bf653445dc66eb
20136819 <20136819@tafe.wa.edu.au>
2025-03-29 12:56:33 +0800

    feat: add 2 files

---
+++ test_tic_tac_bug_toe.py
@@ -0,0 +1,19 @@
+# -------------------------------
+#
+# Folder: 
+# Filename: 
+# Author: Ayano SASAKIDO
+# Version 0.0
+# Created: 
+#
+# -------------------------------
+
+import unittest
+import tic_tac_bug_toe
+
+class TestTicTac(unittest.TestCase):
+    def test_check_win_X(self):
+        board = [['X', 'X', 'X'],
+                 ['O', 'O', ' '],
+                 [' ', ' ', ' ']]
+        self.assertTrue(tic_tac_bug_toe.is_win(board, "X"))
